0743ee8fbc5f3a420de8e992ec5bc7fb691367c0
Jeleen Cubillas <jpclavecilla@gmail.com>
2020-12-17 04:06:46 +0800

    Level 2

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-// Level 1
+// Submit Name Button
+document.getElementById("submit-name").addEventListener("click", function() {
+  const name = document.getElementById("name-field").value;
+  document.getElementById("display-screen").innerHTML = name;
+  });
+
+// Hello Button
 document.getElementById("hello-button").addEventListener("click", function() {
-    document.getElementById("display-screen").innerHTML = "Hello, World!";
+  const name = document.getElementById("name-field").value;
+  document.getElementById("display-screen").innerHTML = "Hello, " + name + "!";
   });
